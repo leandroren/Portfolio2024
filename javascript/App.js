d29af13c8fa5be164f6afce9ecfc1e17767568c6
LeandroRen <103762408+leandroren@users.noreply.github.com>
2024-01-24 21:25:20 -0300

    Portfolio Personal

---
+++ javascript/App.js
@@ -0,0 +1,15 @@
+new TypeIt("#multipleStrings", {
+  strings: [
+    "<b>Hola, Soy Leandro</b>",
+    "<b>Desarrollador Front end.</b>",
+    "<span style='font-size: 18px;'>Actualmente capacitándome en React js.</span>"
+  ],
+  speed: 100,
+  waitUntilVisible: true,
+}).go();
+
+
+// iniciar AOS
+  AOS.init();
+
+
